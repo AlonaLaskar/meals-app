57dda7217c964f296a403c560f7aab8773636949
Alona Laskar <alona159951@gmail.com>
2025-01-01 19:04:44 +0200

    General changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-
 import 'screens/tabs.dart';
 
 final theme = ThemeData(
+  useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
